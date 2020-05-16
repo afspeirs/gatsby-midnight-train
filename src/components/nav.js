0ec369af4f0a506b1c1b26faf2551f5c467d1649
Andy <andy.speirs2@gmail.com>
2020-05-16 19:03:29 +0100

    Updated nav styling
    Removed nav toggle button

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,52 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
 
 import pages from '../pages';
 import { content } from './layout.module.scss';
 import styles from './nav.module.scss';
 
-const Nav = () => {
-  const [toggle, setToggle] = useState(false);
+const Nav = () => (
+  <nav className={`${content} ${styles.nav}`}>
+    <Link
+      className={styles.navLogo}
+      to="/"
+    >
+      Midnight Train
+    </Link>
 
-  const onToggle = () => {
-    setToggle((prevToggle) => !prevToggle);
-  };
-
-  return (
-    <nav className={`${content} ${styles.nav}`}>
-      <Link
-        className={styles.navLogo}
-        to="/"
-      >
-        LOGO
-      </Link>
-
-      <button
-        type="button"
-        className={toggle ? styles.navToggleExpanded : styles.navToggle}
-        aria-controls="menu"
-        aria-expanded={toggle}
-        aria-label="Menu"
-        onClick={onToggle}
-      >
-        <span />
-      </button>
-
-      <ul className={toggle ? styles.navListExpanded : styles.navList}>
-        {pages.map((page) => (
-          <li key={page.slug} className={styles.navListItem}>
-            <Link
-              className={styles.navLink}
-              activeClassName={styles.activeNavLink}
-              to={page.slug}
-            >
-              {page.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
+    <ul className={styles.navList}>
+      {pages.map((page) => (
+        <li key={page.slug} className={styles.navListItem}>
+          <Link
+            className={styles.navLink}
+            activeClassName={styles.activeNavLink}
+            to={page.slug}
+          >
+            {page.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
 
 export default Nav;
