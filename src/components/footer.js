83731fadba4ef3b947d02993dc27168ffa3d8464
Andy <andy.speirs2@gmail.com>
2020-05-16 10:11:42 +0100

    Added dynamic facebook link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,37 +1,53 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 
 import pages from '../pages';
 import styles from './footer.module.scss';
 import { content } from './layout.module.scss';
 
-const Footer = () => (
-  <footer className={styles.footer}>
-    <div className={`${content} ${styles.footerContent}`}>
-      <nav>
-        {pages.map((page, index) => (
-          <>
-            <Link
-              to={page.slug}
-              className={styles.footerLink}
-              activeClassName={styles.activeFooterLink}
-            >
-              {page.title}
-            </Link>
-            {pages.length - 1 !== index ? ' - ' : ''}
-          </>
-        ))}
-      </nav>
-      <span>
-        <a
-          className={styles.footerLink}
-          href="https://www.facebook.com/originalmidnighttrain/"
-        >
-          @originalmidnighttrain
-        </a>
-      </span>
-    </div>
-  </footer>
-);
+const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          social {
+            facebook
+          }
+        }
+      }
+    }
+  `);
+
+  const { facebook } = data.site.siteMetadata.social;
+
+  return (
+    <footer className={styles.footer}>
+      <div className={`${content} ${styles.footerContent}`}>
+        <nav>
+          {pages.map((page, index) => (
+            <>
+              <Link
+                to={page.slug}
+                className={styles.footerLink}
+                activeClassName={styles.activeFooterLink}
+              >
+                {page.title}
+              </Link>
+              {pages.length - 1 !== index ? ' - ' : ''}
+            </>
+          ))}
+        </nav>
+        <span>
+          <a
+            className={styles.footerLink}
+            href={`https://www.facebook.com/${facebook}/`}
+          >
+            {`@${facebook}`}
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
