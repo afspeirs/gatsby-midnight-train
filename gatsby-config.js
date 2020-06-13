4e84ddf253667430374ed89ff5cf9c87e1bd3eae
Andy <andy.speirs2@gmail.com>
2020-06-13 22:57:51 +0100

    Ordered plugins in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,32 +67,25 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          'gatsby-remark-copy-linked-files',
           {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 590,
             },
           },
+          'gatsby-remark-prismjs',
           {
             resolve: 'gatsby-remark-responsive-iframe',
             options: {
               wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
         ],
       },
     },
     'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
     'gatsby-plugin-feed',
     {
       resolve: 'gatsby-plugin-manifest',
@@ -106,8 +99,15 @@ module.exports = {
         icon: 'content/assets/midnight-train-logo.png',
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        // trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
   ],
 };
